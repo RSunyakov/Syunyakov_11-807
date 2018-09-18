26ebb01fc4be161030a9a3bb599641030c63ba29
Roman Syunyakov <romansunyakov@gmail.com>
2018-09-18 08:23:44 +0300

    Lesson 04, HW_3.java updated

--- Lesson04/src/com/company/HW_3.java
+++ Lesson04/src/com/company/HW_3.java
@@ -1,6 +1,5 @@
 package com.company;
 
-
 import java.util.Scanner;
 
 public class HW_3 {
@@ -8,9 +7,9 @@ public class HW_3 {
             Scanner sc = new Scanner(System.in);
             System.out.println("Please, enter a string");
             String s = sc.next();
-        System.out.println("Please, enter start index");
+            System.out.println("Please, enter start index");
             int n1 = sc.nextInt();
-        System.out.println("Please, enter end index");
+            System.out.println("Please, enter end index");
             int n2 = sc.nextInt();
             StringBuilder strResult = new StringBuilder(s);
             strResult.delete(n1,n2 + 1);
