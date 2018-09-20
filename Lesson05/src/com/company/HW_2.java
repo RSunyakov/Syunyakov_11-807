5cfb58e7d6e3e4935a20e0493e996c5466942892
Roman Syunyakov <romansunyakov@gmail.com>
2018-09-20 21:44:04 +0300

    Lesson 05, HW_2.java added

---
+++ Lesson05/src/com/company/HW_2.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class HW_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String s = sc.next(); //почему-то строка считывается только до пробела
+        String[] str = s.split("");
+        for (int i = 0; i < str.length; i++) {
+            if (str [i].equals(String.valueOf(0)) || str [i].equals(String.valueOf(1)) || str [i].equals(String.valueOf(2)) || str [i].equals(String.valueOf(3)) || str [i].equals(String.valueOf(4)) || str [i].equals(String.valueOf(5)) || str [i].equals(String.valueOf(6)) || str [i].equals(String.valueOf(7)) || str [i].equals(String.valueOf(8)) || str[i].equals(String.valueOf(9))) {
+                str[i] = "*";
+            }
+        }
+        s = String.join("", str);
+        System.out.println(s);
+    }
+}
