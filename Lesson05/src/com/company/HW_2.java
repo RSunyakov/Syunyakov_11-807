5752ce0a579d1c5c18fe7f9b306656f7c0a9f890
Roman Syunyakov <romansunyakov@gmail.com>
2018-09-20 22:11:21 +0300

    Lesson 05, HW_2.java updated

--- Lesson05/src/com/company/HW_2.java
+++ Lesson05/src/com/company/HW_2.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class HW_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        String s = sc.next(); //почему-то строка считывается только до пробела
+        System.out.println("Please, enter a string");
+        String s = sc.nextLine();
         String[] str = s.split("");
         for (int i = 0; i < str.length; i++) {
             if (str [i].equals(String.valueOf(0)) || str [i].equals(String.valueOf(1)) || str [i].equals(String.valueOf(2)) || str [i].equals(String.valueOf(3)) || str [i].equals(String.valueOf(4)) || str [i].equals(String.valueOf(5)) || str [i].equals(String.valueOf(6)) || str [i].equals(String.valueOf(7)) || str [i].equals(String.valueOf(8)) || str[i].equals(String.valueOf(9))) {
