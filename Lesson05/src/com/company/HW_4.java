6c38b1ddca2af9f089d868598228f066b9541ae3
Roman Syunyakov <romansunyakov@gmail.com>
2018-09-21 09:55:28 +0300

    Lesson 05, HW_4.java optimized

--- Lesson05/src/com/company/HW_4.java
+++ Lesson05/src/com/company/HW_4.java
@@ -9,21 +9,24 @@ public class HW_4 {
         String s = sc.next();
         System.out.println("Please ,enter a (key)");
         String key = sc.next();
+        int code_of_a = 0;
+        int code_of_z = 0;
+        int register = 0;
         char a[] = s.toCharArray();
         if (key.equals("u")) {
-            for (int i = 0; i < s.length(); i++) {
-                if (a[i] >= 97 & a[i] <= 122) {
-                       int x = a[i] - 32;
-                       a[i] = (char) x;
-                }
-            }
+            code_of_a = 97;
+            code_of_z = 122;
+            register = -32;
         }
         if (key.equals("l")) {
-            for (int i = 0; i < s.length(); i++) {
-                if (a[i] >= 65 & a[i] <= 90) {
-                    int x = a[i] + 32;
-                    a[i] = (char) x;
-                }
+            code_of_a = 65;
+            code_of_z = 90;
+            register = 32;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (a[i] >= code_of_a & a[i] <= code_of_z) {
+                int x = a[i] + register;
+                a[i] = (char) x;
             }
         }
         String out = new String(a);
